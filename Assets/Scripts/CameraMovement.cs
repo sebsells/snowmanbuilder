39c1b6d7e84fd0c0e1fff047935fdbe5c5799885
LudoWest <71764011+LudoWest@users.noreply.github.com>
2020-11-10 16:04:07 +0000

    Camera Movement Added

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Camera mainCamera;
+    public GameObject snowManEmpty;
+    public GameObject[] initializeObjects = new GameObject[10];
+    public Transform[] cameraPositions = new Transform[10];
+    public Transform[] snowManPositions = new Transform[10];
+    int positionCount = 0;
+
+    public void NextGame()
+    {
+        positionCount += 1;
+        mainCamera.transform.position = cameraPositions[positionCount].position;
+        snowManEmpty.transform.position = snowManPositions[positionCount].position;
+        initializeObjects[positionCount].SetActive(true);
+    }
+}
