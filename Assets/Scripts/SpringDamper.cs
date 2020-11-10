8dcb6dbd33fed2808a48f40a40f12c60f9a65f56
LudoWest <71764011+LudoWest@users.noreply.github.com>
2020-11-10 16:21:53 +0000

    SpringDamper for click movement

---
+++ Assets/Scripts/SpringDamper.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SpringDamper : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public Vector2 targetPos;
+    public Vector2 currentPos;
+    public Vector2 direction;
+    public Vector3 screenCenter;
+    public Rigidbody2D rigBod;
+    private float spring = 1f;
+    private float drag = 0.05f;
+    public bool dragging = false;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigBod = this.GetComponent<Rigidbody2D>();
+        screenCenter = new Vector3 (Screen.width/2, Screen.height/2,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentPos = new Vector2(this.transform.localPosition.x, this.transform.localPosition.y);
+        direction = (targetPos - currentPos);
+
+        rigBod.velocity += direction * spring;
+        rigBod.velocity -= rigBod.velocity * drag;
+
+
+        if (dragging)
+        {
+            this.transform.localPosition = Input.mousePosition - screenCenter;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        dragging = true;
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        dragging = false;
+    }
+}
